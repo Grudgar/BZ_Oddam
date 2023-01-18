27a23b8e3bcc6b7be20eb99020cfa6f9b897f82f
Grudgar <bartosz.zychowicz@gmail.com>
2023-01-18 10:16:25 +0100

    fixing urls and making models

--- Oddam_w_dobre_rece/models.py
+++ Oddam_w_dobre_rece/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+TYPE = ((1, "Fundacja"),
+        (2, "Organizacja pozarządowa"),
+        (3, "Zbiórka lokalna"))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(max_length=64, choices=TYPE, default=1)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+
--- Oddam_w_dobre_rece/views.py
+++ Oddam_w_dobre_rece/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Institution, Donation, Category
 
 # Create your views here.
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        bags = Donation.objects.all().count()
+        supported = Institution.objects.all().count()
+        ctx = {
+            "bags": bags,
+            "supported": supported
+        }
+        return render(request, "index.html", ctx)
 
 
 class AddDonation(View):
